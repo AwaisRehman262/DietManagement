46541a090265eec8cae9dc117c58e09aaef439db
AwwaisRehman262 <aw2946652@gmail.com>
2023-08-31 02:09:08 +0500

    some changes

--- Dietmanagement/src/components/Header.jsx
+++ Dietmanagement/src/components/Header.jsx
@@ -6,50 +6,10 @@ import './Header.css'; // Import the external CSS file
 import { Link } from 'react-router-dom';
 
 
-function PositionedMenu() {
-    const [anchorEl, setAnchorEl] = React.useState < null | HTMLElement > (null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-}
-const handleClose = () => {
-        setAnchorEl(null);
-};
-
 const Header = () => {
     return (
         <header className="header">
-                <div>
-                    <Button
-                        id="demo-positioned-button"
-                        aria-controls={open ? 'demo-positioned-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                        onClick={handleClick}
-                    >
-                        Dashboard
-                    </Button>
-                    <Menu
-                        id="demo-positioned-menu"
-                        aria-labelledby="demo-positioned-button"
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu>
-                </div>
+    
             <h1 className="logo">Diet Manager</h1>
             <nav className="nav">
                 <ul className="navList">
