ee96f276c9fda01cf110e3a0d0489afdebf61cc1
AwwaisRehman262 <aw2946652@gmail.com>
2023-08-31 16:53:26 +0500

    bakcend integrated

--- Dietmanagement/vite.config.js
+++ Dietmanagement/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   server: {
     proxy: {
-      '/api' : 'http://localhost:2602'
+      '/api': 'http://localhost:8080'
     }
   },
   plugins: [react()]
